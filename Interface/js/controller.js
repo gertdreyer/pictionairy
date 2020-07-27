b6f31de6acd845194c341c30ef3dfac541ab938c
Ivansteinium <u17018082@tuks.co.za>
2020-07-27 15:35:50 +0200

    Added overlay

---
+++ Interface/js/controller.js
@@ -0,0 +1,16 @@
+
+function disable() {
+  document.getElementById("disableOverlay").style.display = "block";
+}
+
+function enable() {
+  document.getElementById("disableOverlay").style.display = "none";
+}
+
+function openChoose() {
+  document.getElementById("chooseOverlay").style.display = "block";
+}
+
+function closeChoose() {
+  document.getElementById("chooseOverlay").style.display = "none";
+}
