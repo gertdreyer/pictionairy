4ead99d92bb6481308abc76c94fa12a3578f400d
DuncanV <duncanvodden999@gmail.com>
2020-07-27 19:41:58 +0200

    added ability to add guesses, and timer functionality

--- Interface/js/lobby.js
+++ Interface/js/lobby.js
@@ -3,4 +3,29 @@ function init(){
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    var guess = document.getElementById("guessSubmit");
+    guess.addEventListener("keyup", function(event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            guess.value = guess.value.trim();
+            if(guess.value !== ''){
+                document.getElementById('guesses').innerHTML += '&#13;&#10;'+guess.value;
+            }
+            guess.value = '';
+        }
+    });
+}
+function clearGuesses(){
+    document.getElementById("guesses").innerHTML = 'Your Guesses Are Here:&#13;&#10;';
+}
+
+function clearPlayers(){
+    document.getElementById('players').innerHTML += 'Players:&#13;&#10;';
+}
+
+function updateTimer(time){
+    document.getElementById('timer').innerHTML = time;
 }
