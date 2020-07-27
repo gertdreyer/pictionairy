8152565f20284c3711c934b69e1696ff3028e19a
DuncanV <duncanvodden999@gmail.com>
2020-07-27 13:46:39 +0200

    Co-authored-by: Duncan <duncanvodden999@gmail.com>

---
+++ Interface/js/main.js
@@ -0,0 +1,14 @@
+function createRoom(){
+    //TODO create room and all that
+    window.location.href = './html/lobby.html'
+}
+
+function joinRoom(){
+    //TODO
+    window.location.href = './html/lobby.html'
+}
+
+function connectDevice(){
+    //TODO
+    window.location.href = './html/controller.html'
+}
