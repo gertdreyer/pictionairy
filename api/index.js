376faf4e17008450bad364bb322269c3d81b6e80
Gert Dreyer <gertdreyer@gmail.com>
2020-07-28 15:57:25 +0200

    Completed /auth and /register endpoints connected to mongo

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt');
 var server = app.listen(3000)
 var io = require('socket.io')(server);
 const jwt = require("jsonwebtoken")
 const socketioJwt = require('socketio-jwt');
 const { v4: uuidv4 } = require('uuid');
 const port = 3000;
+const saltRounds = 10;
 
 const mongoose = require('mongoose');
 const db_link = "mongodb://vacweekapi.gdza.xyz:27017/db_test";
@@ -20,8 +22,8 @@ mongoose.connect(db_link, (err) => {
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("DB connected")
+db.once('open', function () {
+    console.log("DB connected")
 });
 
 const UserSchema = new mongoose.Schema({
@@ -30,7 +32,8 @@ const UserSchema = new mongoose.Schema({
     previousGames: Object
 })
 
-const User = mongoose.model('User',UserSchema)
+const User = mongoose.model('User', UserSchema)
+
 
 app.use(express.static('public'))
 app.use(express.json());
@@ -38,65 +41,69 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/register', (req, res) => {
-    if (!!req.body.username && !!req.body.password) {
-        return res.json({ registered: true })
+
+/// Registration Endpoint
+app.post('/register', async (req, res) => {
+    try {
+        if (!!req.body.username && !!req.body.password) {
+            // Register new User...
+            if (await User.exists({ username: req.body.username })) {
+                console.log("Existing User")
+                res.json({ error: "Username already exists" })
+            } else {
+                bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+                    // Store hash in your password DB.
+                    const user = new User({
+                        username: req.body.username,
+                        passwordHash: hash,
+                        previousGames: {},
+                    });
+
+                    try {
+                        const newUser = await user.save();
+                        return res.status(200).json({ success: true });
+                    }
+                    catch (err) {
+                        return res.status(500).json({ error: "This shouldn't happen: DB error" })
+                    }
+                    // Need....
+                });
+            }
+        }
+    } catch (err) {
+        return res.json({ error: "body not defined..." })
     }
 });
 
-app.post('/auth', (req, res) => {
+
+/// Authentication Endpoint
+app.post('/auth', async (req, res) => {
     if (!!req.body.username && !!req.body.password) {
-        return res.json({ jwt: jwt.sign({ username: "test", name: "NameOfUser" }, JWTSECRET, { expiresIn: "2 days" }) });
+        // Load hash from your password DB.
+        let user;
+        try {
+            user = await User.findOne({ username: req.body.username });
+            if (user == null) {
+                return res.json({ jwt: null, error: "Username and/or Password is not associated with an account (Err-FCK-210)" });
+            }
+
+            let hash = user.passwordHash;
+            bcrypt.compare(req.body.password, hash, (err, result) => {
+                // result == true
+                console.log(JSON.parse(JSON.stringify(user)));
+                if (result) {
+                    delete user.passwordHash;
+                    return res.json({ jwt: jwt.sign({ username: user.username }, JWTSECRET, { expiresIn: "2 days" }) });
+                } else {
+                    return res.json({ jwt: null, error: "Username and/or Password is not associated with an account (Err-222)" });
+                }
+            });
+        } catch (err) {
+            return res.status(500).json({ error: err.msg });
+        }
     }
 });
-// #region oldrestapi
-// app.post('/newSession', (req, res) => {
-//     if (!!req.body.userId) {
-//         let session = uuidv4();
-//         res.json({ "sessionId": session })
-//         //TODO: Firebase
-//     } else {
-//         res.json({ error: "Missing userId" })
-//     }
-// })
 
-// app.post('/join', (req, res) => {
-//     if (!!req.body.userId && !!req.body.sessionId) {
-//         // Mock method
-//         res.json({ joined: true })
-//     } else {
-//         res.json({ joined: false, error: "Missing sessionId or userId" })
-//     }
-
-//     //joinAsUser (session_id, user_id) return bool
-//     //join as viewer
-//     //Ensure that once the game is started this function always returns false for this session id
-//     // joinAsDevice (session_id, user_id) return bool
-//     //join as device
-//     //Ensure that once the game is started this function always returns false for this session id
-// })
-
-// app.post('/startGame', (req, res) => {
-//     if (!!req.body.userId && !!req.body.sessionId) {
-//         res.json({ started: true });
-//     } else {
-//         res.json({ started: false, error: "Missing sessionId or userId" })
-//     }
-//     //startGame (session_id) return bool
-//     //Set round to 0
-//     //Make sure that all users have both viewing and drawing devices
-// })
-
-// app.post('/guess', (req, res) => {
-//     if (!!req.body.userId && !!req.body.sessionId && !!req.body.guess) {
-//         //GE integration
-//         res.json({ correct: true })
-//     } else {
-//         res.json({ correct: false, error: "Missing sessionId or userId of guess" })
-//     }
-// })
-
-//#endregion
 io.use(socketioJwt.authorize({
     secret: JWTSECRET,
     handshake: true
@@ -109,18 +116,18 @@ io.on('connection', (socket) => {
         roomid = uuidv4()
         console.log("createroom", roomid);
         socket.leaveAll(); //leave all rooms
-        socket.emit("roomid",{roomid: roomid}); //reply with roomid
+        socket.emit("roomid", { roomid: roomid }); //reply with roomid
         socket.join(roomid);
     });
     socket.on('joinroom', (roomid) => {
         console.log("joinroom", roomid);
         socket.leaveAll(); //ensure only one room
         socket.join(roomid)
-        socket.emit("joinroom", {roomid: roomid, joined: true})
+        socket.emit("joinroom", { roomid: roomid, joined: true })
     });
     socket.on('startgame', () => {
         console.log("startgame in room", roomid);
-        socket.to(roomid).emit("startgame", {started: true})
+        socket.to(roomid).emit("startgame", { started: true })
     });
     socket.on('drawdata', (data) => {
         socket.to(roomid).emit(data);
@@ -128,24 +135,24 @@ io.on('connection', (socket) => {
     });
     socket.on('guess', (guess) => {
         console.log("guess");
-        socket.to(roomid).emit({username: "user",guess: "someguess", correct: false});
+        socket.to(roomid).emit({ username: "user", guess: "someguess", correct: false });
     });
 
-    socket.on('newround', ()=> {
-        socket.to(roomid).emit({newround: true})
-        
+    socket.on('newround', () => {
+        socket.to(roomid).emit({ newround: true })
+
     });
 
-    socket.on('startround', (chosenword)=> {
+    socket.on('startround', (chosenword) => {
         console.log(chosenword)
     });
 
-    socket.on('testing', (event)=> {
+    socket.on('testing', (event) => {
         socket.emit("testing", event)
         console.log(event);
-    });    
-    socket.on('broadcast', (event)=> {
-        socket.to(roomid).emit({success:true})
+    });
+    socket.on('broadcast', (event) => {
+        socket.to(roomid).emit({ success: true })
     }
     )
 });
