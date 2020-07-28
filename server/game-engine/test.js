41955f544b38e7e9330c7fe68b24d91234699378
NicPym <nicholasjohnpym@gmail.com>
2020-07-28 16:09:26 +0200

    Fixed async code

--- server/game-engine/test.js
+++ server/game-engine/test.js
@@ -3,63 +3,68 @@ import Game from "./game.js";
 import Word from "./word.js";
 
 // Helper functions
-export function performUnitTest(itemTested, expected, actual) {
-  if (expected == actual) console.log("\u2713  " + itemTested + " passed test");
-  else
-    console.log(
-      "\u2718  " +
-        itemTested +
-        " expected to be: " +
-        expected +
-        " but was " +
-        actual
-    );
+function performUnitTest(itemTested, expected, actual) {
+    if (expected == actual)
+        console.log("\u2713  " + itemTested + " passed test");
+    else
+        console.log(
+            "\u2718  " +
+                itemTested +
+                " expected to be: " +
+                expected +
+                " but was " +
+                actual
+        );
 }
 
 // Player unit tests
-export function playerTest() {
-  console.log("===== Player unit tests =====\n");
-  let player = new Player("nicpym", "Nicholas");
-  performUnitTest("Player ID", "nicpym", player.getPlayerUID());
-  performUnitTest("Player Name", "Nicholas", player.getPlayerName());
-  performUnitTest("Points", 0, player.getPlayerPoints());
-  performUnitTest("Draw Turn Count", 0, player.getDrawTurnCount());
-  performUnitTest(
-    "Player Connection Status",
-    "connected",
-    player.getPlayerConnectionStatus()
-  );
+function playerTest() {
+    console.log("===== Player unit tests =====\n");
+    let player = new Player("nicpym", "Nicholas");
+    performUnitTest("Player ID", "nicpym", player.getPlayerUID());
+    performUnitTest("Player Name", "Nicholas", player.getPlayerName());
+    performUnitTest("Points", 0, player.getPlayerPoints());
+    performUnitTest("Draw Turn Count", 0, player.getDrawTurnCount());
+    performUnitTest(
+        "Player Connection Status",
+        "connected",
+        player.getPlayerConnectionStatus()
+    );
 
-  if (player.getGuesses().length != 0) console.log("Guesses Test Failed");
+    if (player.getGuesses().length != 0) console.log("Guesses Test Failed");
 
-  player.setPlayerPoints(10);
-  performUnitTest("Points", 10, player.getPlayerPoints());
+    player.setPlayerPoints(10);
+    performUnitTest("Points", 10, player.getPlayerPoints());
 
-  player.incrementDrawTurnCount();
-  performUnitTest("Draw Turn Count", 1, player.getDrawTurnCount());
+    player.incrementDrawTurnCount();
+    performUnitTest("Draw Turn Count", 1, player.getDrawTurnCount());
 
-  player.toggleConnection();
-  performUnitTest(
-    "Player Connection Status",
-    "disconnected",
-    player.getPlayerConnectionStatus()
-  );
+    player.toggleConnection();
+    performUnitTest(
+        "Player Connection Status",
+        "disconnected",
+        player.getPlayerConnectionStatus()
+    );
 
-  player.addGuess("block", true);
+    player.addGuess("block", true);
 
-  let guesses = player.getGuesses();
-  if (guesses.length != 1) console.log("Add Guesses Failed");
-  else {
-    performUnitTest("Guess 1 Word", "block", guesses[0].guessedWord);
-    performUnitTest("Guess 1 Correctness", true, guesses[0].guessCorrectness);
-  }
+    let guesses = player.getGuesses();
+    if (guesses.length != 1) console.log("Add Guesses Failed");
+    else {
+        performUnitTest("Guess 1 Word", "block", guesses[0].guessedWord);
+        performUnitTest(
+            "Guess 1 Correctness",
+            true,
+            guesses[0].guessCorrectness
+        );
+    }
 
-  console.log("");
+    console.log("");
 }
 
 // Game unit tests
 
-export function gameTest() {
+async function gameTest() {
     console.log("===== Game unit tests =====\n");
     let game = new Game("game123");
 
@@ -86,76 +91,89 @@ export function gameTest() {
     game = new Game("game123");
     performUnitTest("Start Round Without Players", false, game.startNewRound());
     performUnitTest("Start Turn Without Players", false, game.startNewTurn());
-    for(let i = 0; i < 4; i++){
+    for (let i = 0; i < 4; i++) {
         game.addPlayer(new Player(String(i), String(i)));
     }
     performUnitTest("Start Round With Players", true, game.startNewRound());
     performUnitTest("Start Turn With Players", true, game.startNewTurn());
-    performUnitTest("Current Player Properly Assigned", true, game.getDrawer() != null);
+    performUnitTest(
+        "Current Player Properly Assigned",
+        true,
+        game.getDrawer() != null
+    );
     performUnitTest("Start Round 2", true, game.startNewRound());
     performUnitTest("Start Round 3", true, game.startNewRound());
-    performUnitTest("Attempt To Start Another Round After Max Round Reached", false, game.startNewRound());
+    performUnitTest(
+        "Attempt To Start Another Round After Max Round Reached",
+        false,
+        game.startNewRound()
+    );
 
-    game.generateWords().then((res) => {
-        if (res.length == 3) {
-            performUnitTest("Words generated", 0, 0);
-        }
-    });
+    let words = await game.generateWords();
+    if (words.length == 3) {
+        performUnitTest("Words generated", 0, 0);
+    }
 }
 
 // Word unit tests
-export async function getWordsTest() {
+async function getWordsTest() {
     console.log("\n===== Words unit tests =====\n");
-  let word = new Word();
-  let ExpectedWordsEasy = await word
-    .getWords(1)
-    .then()
-    .catch((err) => {
-      console.log(err);
-    });
-  if (ExpectedWordsEasy.length != 3) {
-    console.log(
-      "\u2718  " +
-        " Easy words get" +
-        " expected to be: 3 but was " +
-        ExpectedWordsEasy.length
-    );
-  } else {
-    console.log("\u2713  " + "Easy words get passed test");
-  }
+    let word = new Word();
+    let ExpectedWordsEasy = await word
+        .getWords(1)
+        .then()
+        .catch((err) => {
+            console.log(err);
+        });
+    if (ExpectedWordsEasy.length != 3) {
+        console.log(
+            "\u2718  " +
+                " Easy words get" +
+                " expected to be: 3 but was " +
+                ExpectedWordsEasy.length
+        );
+    } else {
+        console.log("\u2713  " + "Easy words get passed test");
+    }
 
-  let ExpectedWordsMedium = await word
-    .getWords(2)
-    .then()
-    .catch((err) => {
-      console.log(err);
-    });
+    let ExpectedWordsMedium = await word
+        .getWords(2)
+        .then()
+        .catch((err) => {
+            console.log(err);
+        });
 
-  if (ExpectedWordsMedium.length != 3) {
-    console.log(
-      "\u2718  " +
-        " Medium words get" +
-        " expected to be: 3 but was " +
-        ExpectedWordsMedium.length
-    );
-  } else {
-    console.log("\u2713  " + "Medium words get passed test");
-  }
+    if (ExpectedWordsMedium.length != 3) {
+        console.log(
+            "\u2718  " +
+                " Medium words get" +
+                " expected to be: 3 but was " +
+                ExpectedWordsMedium.length
+        );
+    } else {
+        console.log("\u2713  " + "Medium words get passed test");
+    }
 
-  let ExpectedWordsHard = await word
-    .getWords(3)
-    .then()
-    .catch((err) => {
-      console.log(err);
-    });
-  if (ExpectedWordsHard.length != 3) {
-    console.log(
-      "\u2718  " +
-        " Medium words get" +
-        " expected to be: 3 but was " +
-        ExpectedWordsHard.length
-    );
-  } else {
-    console.log("\u2713  " + "Medium words get passed test");
-  }
+    let ExpectedWordsHard = await word
+        .getWords(3)
+        .then()
+        .catch((err) => {
+            console.log(err);
+        });
+    if (ExpectedWordsHard.length != 3) {
+        console.log(
+            "\u2718  " +
+                " Medium words get" +
+                " expected to be: 3 but was " +
+                ExpectedWordsHard.length
+        );
+    } else {
+        console.log("\u2713  " + "Medium words get passed test");
+    }
+}
+
+export async function test() {
+    playerTest();
+    await gameTest();
+    await getWordsTest();
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 import { createRequire } from "module";
-const require = createRequire( import.meta.url);
-require('dotenv').config();
-import { playerTest, gameTest,getWordsTest } from './game-engine/test.js';
-import uploadWords from './game-engine/upload_words.js';
+const require = createRequire(import.meta.url);
+require("dotenv").config();
+import { test } from "./game-engine/test.js";
+import uploadWords from "./game-engine/upload_words.js";
 //  uploadWords();   //T upload words to the Google Firestore
 
-playerTest();
-gameTest();
-getWordsTest();
+test();
