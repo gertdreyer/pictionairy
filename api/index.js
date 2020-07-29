fbf003f3d65872b5bf76c87493554ee8b1fddccd
Gert Dreyer <gertdreyer@gmail.com>
2020-07-29 11:04:55 +0200

    First sprint

--- api/index.js
+++ api/index.js
@@ -1,21 +1,26 @@
 import express from 'express'
-const app = express()
 import bcrypt from 'bcrypt';
-var server = app.listen(3000)
-var io = require('socket.io')(server);
+
 import jwt from "jsonwebtoken";
 import cors from 'cors';
-import socketioJwt from 'socketio-jwt';
-import { v4 as uuidv4 } from 'uuid';
 import mongoose from 'mongoose';
-import Game from "../server/game-engine/game.js";
+import { User } from './db.js';
+const db_link = process.env.DB_HOST || "mongodb://vacweekapi.gdza.xyz:27017/db_test";
+// App Config.
 const port = 3000;
 const saltRounds = 10;
+const JWTSECRET = process.env.JWT_SECRET || "this-should-be-some-super-secret-key";
 
-const db_link = process.env.DB_HOST || "mongodb://vacweekapi.gdza.xyz:27017/db_test";
-// const JWTSECRET = process.env.JWT_SECRET || "this-should-be-some-super-secret-key";
-const JWTSECRET = "this-should-be-some-super-secret-key";
+const app = express()
+var server = app.listen(port)
+var io = require('socket.io')(server, { 'pingInterval': 1000, 'maxHttpBufferSize': "10e7" });
+const socketserver = require('./socketserver.js')(io);
 
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.json());
+
+// Database Initiation
 mongoose.connect(db_link, (err) => {
     if (err)
         console.log("Error");
@@ -29,43 +34,35 @@ db.once('open', function () {
     console.log("DB connected")
 });
 
-const GameSchema = new mongoose.Schema({
-    gameid: String,
-    iat: Date,
-    gamestate: Object
-})
-
-const UserSchema = new mongoose.Schema({
-    username: String,
-    name: String,
-    passwordHash: String,
-    previousGames: Object
-})
-
-const User = mongoose.model('User', UserSchema);
-const GameState = mongoose.model('GameState', GameSchema);
-
-app.use(express.static('public'))
-app.use(express.json());
+// TODO: Move endpoints to routes...
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-app.options('*', cors())
 
 /// Registration Endpoint
+/**
+ * @param newUsername the entered new username
+ * @param newPassword the entered new password
+ * @param newName the entered new name
+ */
+
 app.post('/register', async (req, res) => {
     try {
-        if (!!req.body.username && !!req.body.password && !!req.body.name) {
+        let newUsername = req.body.username;
+        let newPassword = req.body.password;
+        let newName = req.body.name;
+
+        if (!!newUsername && !!newPassword && !!newName) {
             // Register new User...
-            if (await User.exists({ username: req.body.username })) {
+            if (await User.exists({ username: newUsername })) { // the user already exists
                 console.log("Existing User")
                 res.json({ error: "Username already exists" })
             } else {
-                bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+                bcrypt.hash(newPassword, saltRounds, async function (err, hash) {
                     // Store hash in your password DB.
                     const user = new User({
-                        name: req.body.name,
-                        username: req.body.username,
+                        name: newName,
+                        username: newUsername,
                         passwordHash: hash,
                         previousGames: {},
                     });
@@ -88,14 +85,23 @@ app.post('/register', async (req, res) => {
 
 
 /// Authentication Endpoint
+/**
+ * @param authUsername the username that needs to be authenticated
+ * @param authPassword the password that needs to be authenticated
+ */
 app.post('/auth', async (req, res) => {
-    if (!!req.body.username && !!req.body.password) {
+    let authUsername = req.body.username;
+    let authPassword = req.body.password;
+
+    if (!!authUsername && !!authPassword) {
         // Load hash from your password DB.
         let user;
         try {
-            user = await User.findOne({ username: req.body.username });
+            user = await User.findOne({ username: authUsername });
+
+            //if the user does not exist
             if (user == null) {
-                return res.json({ jwt: null, error: "Username and/or Password is not associated with an account (Err-FCK-210)" });
+                return res.json({ jwt: null, error: "Username and/or Password is not associated with an account." });
             }
 
             let hash = user.passwordHash;
@@ -106,7 +112,7 @@ app.post('/auth', async (req, res) => {
                     delete user.passwordHash;
                     return res.json({ jwt: jwt.sign({ username: user.username }, JWTSECRET, { expiresIn: "2 days" }) });
                 } else {
-                    return res.json({ jwt: null, error: "Username and/or Password is not associated with an account (Err-222)" });
+                    return res.json({ jwt: null, error: "Username and/or Password is not associated with an account" });
                 }
             });
         } catch (err) {
@@ -115,119 +121,4 @@ app.post('/auth', async (req, res) => {
     }
 });
 
-io.use(socketioJwt.authorize({
-    secret: JWTSECRET,
-    handshake: true
-}));
-
-io.on('connection', (socket) => {
-    let gameid = Object.keys(socket.rooms).filter(item => item != socket.id)[0];
-    let { username, name } = socket.decoded_token;
-    console.log(username, " connected");
-
-    socket.on('newgame', async () => {
-        console.log(gameid);
-        gameid = uuidv4(); //use shorter
-        socket.leaveAll(); //leave all rooms
-
-        // Create Game Object
-        let gamestate = new Game(gameid);
-        // add player
-        gamestate.addPlayer(username, name);
-        let retobj = { ...gamestate };
-        delete retobj.currentWord;
-        delete retobj.wordGenerator;
-
-        //join socket room
-        socket.join(gameid);
-
-        // Save Game instance
-        const newgamestate = new GameState({
-            gameid: gameid,
-            iat: new Date(),
-            gamestate: gamestate,
-        });
-
-
-        try {
-            await newgamestate.save();
-            socket.emit("gamestate", { gamestate: retobj });
-        } catch (err) {
-            socket.emit("gamestate", { error: "Error while creating game", gamestate: null });
-        }
-        //reply with public gamestate
-    });
-
-
-
-    socket.on('joingame', async (obj) => {
-        try {
-            let { gameid } = obj;
-            console.log(gameid);
-            // Find the current game state
-            let { gamestate } = await GameState.findOne({ gameid: gameid });
-
-            //ensure only one socket room and join
-            socket.leaveAll();
-            socket.join(gameid)
-
-            // Add Player
-            //TODO: GameEngine needs constructor overload.
-            // gamestate.addPlayer(username,name)
-            console.log(gamestate);
-
-            socket.to(gameid).emit("gamestate", { gamestate: gamestate })
-        } catch (error) {
-            console.log(error);
-            socket.emit("gamestate", { error: 'Soz...', gamestate: null })
-        }
-    });
-
-
-    socket.on('startnewround', () => {
-        //Host check
-        if (!!gameid) {
-            let game = new Game(); 
-            game.startNewRound()
-            // Check that each player has an assigned controller.game
-            console.log("startgame in room", gameid);
-            socket.to(gameid).emit("startgame", { started: true })
-        }
-    });
-
-
-    socket.on('guess', (guess) => {
-        console.log("guess");
-        socket.to(gameid).emit({ username: "user", guess: "someguess", correct: false });
-    });
-
-    socket.on('newround', () => {
-        socket.to(gameid).emit({ newround: true })
-
-    });
-
-    socket.on('startround', (chosenword) => {
-        console.log(chosenword)
-    });
-
-    socket.on('testing', (event) => {
-        socket.emit("testing", event)
-        console.log(event);
-    });
-
-    socket.on('jointestroom', (event) => {
-        socket.join('test');
-    })
-    socket.on('drawdata', (data) => {
-        socket.to(gameid).emit("drawdata", data);
-        console.log("drawdata", data);
-    });
-    socket.on('broadcast', (event) => {
-        socket.to(gameid).emit({ success: true })
-    }
-    )
-});
-
-io.on('disconnect', (socket) => {
-    console.log('user disconnected');
-});
+export default server
